197bad6d053a07ac501be6d1d0b689a5592239c2
vanessaladiocha2204 <103755396+vanessaladiocha2204@users.noreply.github.com>
2022-07-16 19:40:45 +0700

    CountDownView
    Text Masih Statis

---
+++ PocketKitchen WatchKit Extension/CountDownView.swift
@@ -0,0 +1,73 @@
+//
+//  CountDownView.swift
+//  PocketKitchen WatchKit Extension
+//
+//  Created by Vanessa Ladiocha on 15/07/22.
+//
+
+import SwiftUI
+
+struct CountDownView: View {
+    @State var timeVal = 3
+    @State var progress : Float = 0.0
+    var fill : Float = 0.33
+    
+    var body: some View {
+        VStack {
+            Text("Boiled Egg")
+                .font(.system(size: 16))
+                .bold()
+                .padding(.bottom, 20)
+                
+            ZStack {
+            
+                Text(progress == 0.0
+                     ? "Ready"
+                     : "\(timeVal)")
+                    .bold()
+                    .font(progress == 0.0
+                          ? .system(size: 15)
+                          : .system(size: 60))
+                    .padding()
+                    .onAppear(){
+                        Timer.scheduledTimer(withTimeInterval: 1.0,  repeats: true) { _ in
+                            if timeVal > 1 {
+                                if (progress != 0) {
+                                    timeVal -= 1
+                                }
+                                if (progress) < 1.0 {
+                                    progress += fill
+                                }
+                            } else {
+                                // pindah screen
+                            }
+                            
+                            
+                        }
+                }
+                    
+            
+                Circle()
+                    .stroke(lineWidth: 6.0)
+                    .opacity(1)
+                    .foregroundColor(Color.gray)
+                    .frame(width: 87, height: 87)
+                
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
+                    .stroke(style: StrokeStyle(lineWidth: 6.0, lineCap: .round, lineJoin: .round))
+                    .frame(width: 87, height: 87)
+                    .rotationEffect(Angle(degrees: 270))
+                    .animation(.easeInOut(duration: 0.5))
+                    .foregroundColor(.orange)
+            }
+        }
+    }
+
+}
+
+struct CountDownView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountDownView()
+    }
+}
